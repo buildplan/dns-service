a7ebf16890fa891ef4293bbd6bb79d6919f003ae
buildplan <hello@buildplan.org>
2026-01-09 12:47:34 +0000

    fix regex

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
